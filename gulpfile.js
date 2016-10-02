25f1e55347779b0e2a295c4cd5b0763192590710
Jack Mocherman <mochermanjack@gmail.com>
2016-10-01 23:03:26 -0500

    built gulp bundle and copy tasks

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp')
+const browserify = require('browserify')
+const reactify = require('reactify')
+const source = require('vinyl-source-stream')
+
+// Initiate gulp task 'bundle'
+gulp.task('bundle', function() {
+    return browserify({
+      entries: "./app/main.jsx",
+      debug: true
+    })
+    .transform(reactify)
+      .bundle()
+      .pipe(source('main.js'))
+      .pipe(gulp.dest('app/dist'))
+})
+
+gulp.task('copy', ['bundle'], function() {
+  return gulp.src(['app/index.html', 'app/lib/bootstrap-css/css/bootstrap.min.css', 'app/style.css'])
+    .pipe(gulp.dest('app/dist'))
+})
+
+gulp.task('default', ['copy'], function() {
+  console.log('Gulp Completed')
+})
